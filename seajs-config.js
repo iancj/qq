e93ceff2a0d7e280a323fc5d8671bd0c226aa73d
iancj <ianchenjie@gmail.com>
2014-12-12 09:00:32 +0800

    删除多余的文件

--- seajs-config.js
+++ seajs-config.js
@@ -9,7 +9,7 @@ seajs.config({
 		"zepto":"gallery/zepto/zepto-min.js",
 		"mobilebone":"gallery/mobilebone/mobilebone-min.js",
 		"underscore":"gallery/underscore/1.6.0/underscore.js",
-		"iscroll":"gallery/iscroll/iscroll-infinite.js",
+		"iscroll":"gallery/iscroll/iscroll-probe.js",
 		"LoadCss":"seajs/seajs-css/1.0.4/seajs-css.js",
 		//module
 		"app":"js/app",
